62bc408cf880f8c32ea922c96986f792d7fa632a
michaelippolito826 <michaelippolito826@gmail.com>
2021-07-01 06:32:49 -0400

    working POST

--- music_controller/api/serializers.py
+++ music_controller/api/serializers.py
@@ -10,4 +10,4 @@ class RoomSerializer(serializers.ModelSerializer):
 class CreateRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ('guests_can_pause', 'votes_to_skip ')
+        fields = ('guest_can_pause', 'votes_to_skip')
