2811fd2b0ae734d7112df1afa963ae0dcc390755
michaelippolito826 <michaelippolito826@gmail.com>
2021-07-02 19:00:45 -0400

    YEP CLOCK

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
-import Button from "@material-ui/core/Button"
+import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import { Typography } from "@material-ui/core/Grid";
 import { TextField } from "@material-ui/core/TextField";
 import { FormHelperText } from "@material-ui/core/FormHelperText";
 import { FormControl } from "@material-ui/core/FormControl";
+import { Link } from "react-router-dom";
+import { Radio } from "@material-ui/core/Radio";
+import { FormControlLabel } from "@material-ui/core/FormControlLabel";
 
 export default class CreateRoomPage extends Component {
+  defaultValue = 2;
+
   constructor(props) {
     super(props);
   }
   render() {
-    return <p>This is the Create Room Page</p>;
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography></Typography>
+        </Grid>
+      </Grid>
+    );
   }
 }
