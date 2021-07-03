8c0e9098d7f6db1b866c562694b74d31b6ea8cd9
michaelippolito826 <michaelippolito826@gmail.com>
2021-07-03 17:05:23 -0400

    more changes for the create a room

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import CreateRoomPage from "./CreateRoomPage";
 import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import Room from "./Room";
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,15 +14,17 @@ export default class HomePage extends Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     return (
       <Router>
         <Switch>
           <Route exact path="/">
-            <p>Home Page</p>
+            <p>This is the home page</p>
           </Route>
           <Route path="/join" component={RoomJoinPage} />
           <Route path="/create" component={CreateRoomPage} />
+          <Route path="/room/:roomCode" component={Room} />
         </Switch>
       </Router>
     );
