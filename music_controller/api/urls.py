5f94b424ad450755bc7c4944af182669d8eb7057
michaelippolito826 <michaelippolito826@gmail.com>
2021-07-06 16:34:17 -0400

    update page (not working yet)

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('get-room', GetRoom.as_view()),
     path('join-room', JoinRoom.as_view()),
     path('user-in-room', UserInRoom.as_view()),
-    path('leave_room', LeaveRoom.as_view()),
-    path('update_room', UpdateRoom.as_view())
+    path('leave-room', LeaveRoom.as_view()),
+    path('update-room', UpdateRoom.as_view())
 ]
